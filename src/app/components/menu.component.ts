58a278e40e94b33e78f48a1d6de97b34c6b8bc06
Maël Gaonach <mael@gaonach.com>
2018-03-15 17:52:03 +0100

    refactoring

--- src/app/components/menu.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'menu',
-    templateUrl: '../templates/menu.component.html',
-    styleUrls: ['../templates/menu.component.css']
-})
-export class MenuComponent {
-}

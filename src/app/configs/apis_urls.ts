855343f2341d533650e491a745c1a1875c599a5b
Maël Gaonach <mael@gaonach.com>
2018-03-16 20:01:33 +0100

    Modification des adresses API

--- src/app/configs/apis_urls.ts
+++ src/app/configs/apis_urls.ts
@@ -1,19 +1,19 @@
 
 
 //---------USERS---------
-export const API_USERS = "http://localhost:7777";
+export const API_USERS = "http://gi1.univ-lr.fr:7777";
 export const API_USERS_LOGIN = API_USERS+"/account/login";
 export const API_USERS_REGISTER = API_USERS+"/account/register";
 
 
 
 //---------GAME---------
-export const API_GAME = "http://localhost:7777";
+export const API_GAME = "http://gi1.univ-lr.fr:7777";
 export const API_GAME_DOWNLOAD = API_GAME+"/download/";
 
 
 
 //---------CARTES---------
-export const API_CARDS = "http://localhost:82";
+export const API_CARDS = "http://gi1.univ-lr.fr:82";
 export const API_CARDS_LISTE_IMAGES = API_CARDS+"/images";
 export const API_CARDS_IMAGE = API_CARDS+"/image/";

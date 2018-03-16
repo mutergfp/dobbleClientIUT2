bc9a226e6313609046c8485b6374dd2514b0e4a1
Maël Gaonach <mael@gaonach.com>
2018-03-16 19:50:56 +0100

    Implémentation du leaderboard & gestion des joueurs

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import {PlayerService} from "../services/player/player.service";
+import {Player} from "../services/player/player";
+
+
+
 
 @Component({
-  selector: 'app-leaderboard',
-  templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.css']
+    selector: 'app-leaderboard',
+    templateUrl: './leaderboard.component.html',
+    styleUrls: ['./leaderboard.component.css']
 })
 export class LeaderboardComponent implements OnInit {
 
-  constructor() { }
+    readonly WINS:string = "WINS";
+    readonly LOSSES:string = "LOSSES";
+    readonly RATIO:string = "RATIO";
 
-  ngOnInit() {
-  }
+    sortMethod:string;
 
+    sortInverted:boolean;
+
+    constructor(public playerService:PlayerService) { }
+
+    ngOnInit() {
+        this.playerService.refresh();
+        this.sortMethod=this.RATIO;
+        this.sortInverted=false;
+    }
+
+    get players(){
+
+        let res:Player[] = [];
+
+        switch (this.sortMethod){
+            case this.WINS :
+                res = this.playerService.getPlayersOrderedByWins();
+                break;
+            case this.LOSSES:
+                res = this.playerService.getPlayersOrderedByLosses();
+                break;
+            default:
+                res = this.playerService.getPlayersOrderedByRatio();
+                break
+        }
+
+        if(this.sortInverted){
+            res = res.reverse();
+        }
+        return res
+    }
+
+    setSortMethod(method:string){
+        if(this.sortMethod == method){
+            this.sortInverted = !this.sortInverted;
+        }else{
+            this.sortMethod = method;
+            this.sortInverted = false;
+        }
+    }
+
+    getRank(player:Player){
+        if(this.sortInverted){
+            return this.players.length-this.players.indexOf(player);
+        }else{
+            return this.players.indexOf(player)+1;
+        }
+    }
 }
--- src/app/services/player/player.service.ts
+++ src/app/services/player/player.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Player} from "./player";
 
 @Injectable()
 export class PlayerService {
 
-  constructor() { }
+  players:Player[] = [];
+
+  constructor() {
+    this.refresh();
+  }
+
+  public refresh(){
+    PlayerService.getPlayersList().subscribe(res =>{
+      let players:Player[] = [];
+      for(let p of res){
+        players.push(new Player(p.username,p.wins,p.losses));
+      }
+      this.players=players;
+    });
+  }
+
+  private static getPlayersList():Observable<Player[]>{
+    return Observable.of([{username:"420 Blaze me",wins:57,losses:4},{username:"XXXDarkLucHacker",wins:27,losses:20},{username:"TerraxX",wins:54,losses:2},{username:"Joypok",wins:4,losses:42}]).delay(3000);
+  }
+
+  public getPlayer(username:string):Player{
+    for(let p of this.players){
+      if(p.username==username){
+        return p;
+      }
+    }
+    return undefined;
+  }
+
+  public getPlayersOrderedByWins(){
+    let ordered = this.players;
+    return ordered.sort( (a, b) => {
+      return b.wins - a.wins;
+    });
+  }
+
+  public getPlayersOrderedByRatio(){
+    let ordered = this.players;
+    return ordered.sort( (a, b) => {
+      return b.ratio - a.ratio;
+    });
+  }
+
+  public getPlayersOrderedByLosses(){
+    let ordered = this.players;
+    return ordered.sort( (a, b) => {
+      return b.losses - a.losses;
+    });
+  }
 
 }

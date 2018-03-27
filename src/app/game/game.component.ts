f85b63ad50ce27fbb4fe799c15492dcfd56881b2
Maël Gaonach <mael@gaonach.com>
2018-03-27 15:10:14 +0200

    correction

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {API_GAME_DOWNLOAD} from "../configs/apis_urls";
 import {AuthService} from "../services/auth/auth.service";
 
+let require:any;
+
 @Component({
     selector: 'game',
     templateUrl: 'game.component.html',
